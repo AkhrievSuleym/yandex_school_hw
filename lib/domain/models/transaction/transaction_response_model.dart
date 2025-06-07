2eccc52e29cfceb2e83caed762c63b1c712edc3e
AkhrievSuleym <murid2005@mail.ru>
2025-06-07 22:46:33 +0300

    add transaction models

---
+++ lib/domain/models/transaction/transaction_response_model.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:yandex_shmr_hw/domain/models/account/account_brief_model.dart';
+import 'package:yandex_shmr_hw/domain/models/category/category_model.dart';
+
+part 'transaction_response_model.freezed.dart';
+part 'transaction_response_model.g.dart';
+
+@freezed
+class TransactionResponseModel with _$TransactionResponseModel {
+  @JsonSerializable(explicitToJson: true)
+  const factory TransactionResponseModel({
+    required int id,
+    required AccountBriefModel account,
+    required CategoryModel category,
+    required DateTime transactionDate,
+    String? comment,
+    required DateTime createdAt,
+    required DateTime updatedAt,
+  }) = _TransactionResponseModel;
+
+  factory TransactionResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$TransactionResponseModelFromJson(json);
+}
