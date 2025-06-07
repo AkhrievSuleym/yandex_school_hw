a816ba5d783a6af769fb7e9443f35b750d667075
AkhrievSuleym <murid2005@mail.ru>
2025-06-07 21:49:39 +0300

    add account_model

---
+++ lib/domain/models/account/account_model.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'account_model.freezed.dart';
+part 'account_model.g.dart';
+
+enum Currency { rub, usd, eur }
+
+@freezed
+class AccountModel with _$AccountModel {
+  const factory AccountModel({
+    required int id,
+    required int userId,
+    required String name,
+    required String balance,
+    required Currency currency,
+    required DateTime createdAt,
+    required DateTime updatedAt,
+  }) = _AccountModel;
+
+  factory AccountModel.fromJson(Map<String, dynamic> json) =>
+      _$AccountModelFromJson(json);
+}
