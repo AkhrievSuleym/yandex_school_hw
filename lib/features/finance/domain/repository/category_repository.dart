6453a4557646229c2dd9e5434a672801f7fbde00
AkhrievSuleym <murid2005@mail.ru>
2025-06-12 17:32:37 +0300

    changes in CategoryRepository interface to domain layer

--- lib/features/finance/domain/repository/category_repository.dart
+++ lib/features/finance/domain/repository/category_repository.dart
@@ -1,9 +1,10 @@
-import 'package:either_dart/either.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:yandex_shmr_hw/core/error/failure.dart';
 import 'package:yandex_shmr_hw/features/finance/data/models/category/category_model.dart';
 
 abstract interface class CategoryRepository {
   Future<Either<Failure, List<CategoryModel>>> getAllCategories();
-  Future<Either<Failure, List<CategoryModel>>> getIncomeCategories();
-  Future<Either<Failure, List<CategoryModel>>> getExpenseCategories();
+  Future<Either<Failure, List<CategoryModel>>> getCategoriesByType(
+    bool isIncome,
+  );
 }
